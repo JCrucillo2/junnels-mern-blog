9ea3a59e142052d61d108e186619f7835d380662
Junnel Crucillo <junnelcrucillo729@gmail.com>
2024-03-09 15:56:35 -0700

    installed react and tailwindcss for project

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+	return (
+		<div>
+			<h1>Hello World</h1>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita provident iste debitis impedit quasi, voluptate ad consectetur, nulla beatae consequatur repudiandae voluptas totam numquam reiciendis similique delectus, eos non maxime?</p>
+		</div>
+	);
+}
