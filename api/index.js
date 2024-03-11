863f007f7b3f9ba9db16e87882f1b734f61955ef
Junnel Crucillo <junnelcrucillo729@gmail.com>
2024-03-11 12:09:33 -0600

    create signup api route

--- api/index.js
+++ api/index.js
@@ -3,17 +3,20 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 mongoose
 	.connect(process.env.MONGO)
 	.then(() => console.log("MongoDB is connected"))
 	.catch((err) => console.log(err));
 
-app.listen(8000, () => {
-	console.log("Server is running on port 8000");
+app.listen(process.env.PORT, () => {
+	console.log(`Server is running on port ${process.env.PORT}`);
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
